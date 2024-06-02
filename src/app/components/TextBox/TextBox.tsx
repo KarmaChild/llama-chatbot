f2d0df9493aec43d02238d7769da547aba8903d6
Adithya Mattappily <74791365+KarmaChild@users.noreply.github.com>
2024-06-02 12:23:15 -0600

    modify chat ui

--- src/app/components/TextBox/TextBox.tsx
+++ src/app/components/TextBox/TextBox.tsx
@@ -1,21 +1,48 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
-export const TextBox = () => {
-    const [text, setText] = useState('')
+interface TextBoxProps {
+    onSendMessage: (message: string) => void
+}
 
-    const handleChange = (event: any) => {
+export const TextBox: React.FC<TextBoxProps> = ({ onSendMessage }) => {
+    const [text, setText] = useState("")
+    const [rows, setRows] = useState(0)
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setText(event.target.value)
+        const textareaLineHeight = 24
+        const previousRows = event.target.rows
+        event.target.rows = 1
+        const currentRows = Math.ceil(event.target.scrollHeight / textareaLineHeight)
+        event.target.rows = currentRows
+        setRows(currentRows)
+        if (currentRows !== previousRows) {
+            event.target.scrollTop = event.target.scrollHeight
+        }
+    }
+
+    const handleKeyPress = (
+        event: React.KeyboardEvent<HTMLTextAreaElement>
+    ) => {
+        if (event.key === "Enter" && text.trim() !== "") {
+            onSendMessage(text)
+            setText("")
+            setRows(0)
+        }
     }
 
     return (
-        <div className="bg-primary w-[740px] max-w-full h-auto rounded-[10px] border-2 border-white">
+        <div className="bg-primary w-[740px] max-w-full rounded-[10px] border-[1px] border-amber-50">
             <textarea
-                className="w-full h-full resize-y p-2 text-white bg-transparent outline-none" // Apply Tailwind classes for styling
+                className="w-full h-full resize-none overscroll-none p-2 text-white bg-transparent outline-none"
                 value={text}
                 onChange={handleChange}
-                placeholder="Enter text here..."
-                rows={1} // Start with one row
+                onKeyDown={handleKeyPress}
+                placeholder="Start Chatting..."
+                rows={rows}
             />
         </div>
     )
 }
+
+export default TextBox
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,55 @@
 'use client'
-import {TextBox} from "@/app/components/TextBox/TextBox";
+import {TextBox} from "@/app/components/TextBox/TextBox"
+import {useState} from "react"
 
+interface Message {
+    sent_by: string
+    message: string
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-          <TextBox/>
-      </div>
-    </main>
-  )
+    const [messages, setMessages] = useState<Message[]>([
+        {
+            sent_by: 'user',
+            message: 'Hello'
+        },
+        {
+            sent_by: 'gpt',
+            message: 'Hello! How are you doing today?'
+        },
+        {
+            sent_by: 'user',
+            message: 'Hello'
+        },
+        {
+            sent_by: 'gpt',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer urna leo, luctus vitae orci vitae, ' +
+                'posuere accumsan magna. Vestibulum non cursus ex. Ut finibus sem sit amet ultrices lobortis. Nullam dapibus quis leo quis commodo. Proin nec venenatis ligula, congue auctor massa. In hac habitasse platea dictumst. Pellentesque orci nisl, tempus nec nisl vel, fermentum luctus lectus. Sed rutrum est id tristique ultricies. Morbi at sollicitudin velit, vitae dapibus arcu. Nunc euismod euismod libero, a pretium sem scelerisque in. Ut consectetur, lorem nec ultrices commodo, ' +
+                'diam tortor suscipit ex, vitae lobortis velit metus eget nulla. Fusce convallis eu mauris pulvinar finibus. ' +
+                'Proin consectetur laoreet lacus, eu ullamcorper magna porttitor vitae. Vivamus odio orci, imperdiet ut mauris non, ' +
+                'tempor auctor ipsum. Phasellus sodales odio ac dui posuere, in malesuada urna placerat. Nullam rhoncus, eros non ' +
+                'pellentesque maximus, erat felis vulputate orci, non vestibulum nisl odio quis leo.'
+        }
+    ])
+
+    const addMessage = (message: any) => {
+        setMessages([...messages, message])
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
+            <div className="flex flex-col items-center justify-center h-full w-full" style={{paddingLeft: "30px"}}>
+                {messages.map((message, index) => (
+                    <div key={index} className={`p-2 m-2 bg-primary w-[740px] max-w-full rounded-[10px]`}>
+                        <p className={`${message.sent_by === 'gpt' ? 'text-green-500': 'text-white'}`}>{message.message}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="absolute bottom-[30px]">
+                <TextBox onSendMessage={addMessage}/>
+            </div>
+        </main>
+
+
+    )
 }
